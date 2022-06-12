b9387f0ff9a888ca2e1476db3e3c4fe8575b087a
pablo8 <pablo.ochoteco@gmail.com>
2022-06-12 15:32:49 -0300

    change db_host in constant.py

--- constants.py
+++ constants.py
@@ -1,5 +1,5 @@
 # Heroku DB
-DB_HOST = 'postgres://xxwrqakfncitpi:394df6de511b6fc22049c08e947198703de40d549b7864d8d91414a8497772a8@ec2-54-147-33-38.compute-1.amazonaws.com:5432/d22tmbm98j61ev'
+DB_HOST = 'ec2-54-147-33-38.compute-1.amazonaws.com'
 DB_PORT = '5432'
 DB_NAME = 'd22tmbm98j61ev'
 DB_USER = 'xxwrqakfncitpi'

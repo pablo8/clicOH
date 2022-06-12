ac5fbc6a3e1b99ecc4987dad8a705967203d86a6
pablo8 <pablo.ochoteco@gmail.com>
2022-06-12 15:14:37 -0300

    add confi database

---
+++ constants.py
@@ -0,0 +1,13 @@
+# Heroku DB
+DB_HOST = 'postgres://xxwrqakfncitpi:394df6de511b6fc22049c08e947198703de40d549b7864d8d91414a8497772a8@ec2-54-147-33-38.compute-1.amazonaws.com:5432/d22tmbm98j61ev'
+DB_PORT = '5432'
+DB_NAME = 'd22tmbm98j61ev'
+DB_USER = 'xxwrqakfncitpi'
+DB_PASSWORD = '394df6de511b6fc22049c08e947198703de40d549b7864d8d91414a8497772a8'
+
+# Local DB
+#DB_HOST = 'localhost'
+#DB_PORT = '5432'
+#DB_NAME = 'clicOH'
+#DB_USER = 'postgres'
+#DB_PASSWORD = '123'
